c0eadf4975140d2942ff049160388ff85dd8a37a
Alvajoy Asante <33734347+Overload02@users.noreply.github.com>
2023-05-25 12:06:55 -0400

    started developing saving system

---
+++ src/save.c
@@ -0,0 +1,19 @@
+#include "save.h"
+#include "ui.h"
+
+static void init_settings(void)
+{
+}
+
+void save_game(void)
+{
+    uint8_t var;
+
+}
+
+void load_game(void)
+{
+    uint8_t var;
+
+
+}
---
+++ src/save.h
@@ -0,0 +1,13 @@
+#ifndef SAVE_H
+#define SAVE_H
+
+#include <tice.h>
+#include <fileioc.h>
+
+#define SAVE_APPVAR_NAME DCKHNT
+
+void save_game(void);
+
+void load_game(void);
+
+#endif
